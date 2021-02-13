b879e80646428705ee2d2664f4a3f818c6211cde
Revaan Mishra <76741350+revaan-mishra@users.noreply.github.com>
2021-02-14 02:58:51 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (80.33%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,29 +1,19 @@
-class Solution
-{
-	public:
-		void sortColors(vector<int> &nums)
-		{
-			int index {0};
-			int start {0};
-			int end = nums.size() - 1;
-
-			while (index <= end && start < end)
-			{
-				if (nums.at(index) == 0)
-				{
-					nums.at(index) = nums.at(start);
-					nums.at(start) = 0;
-					start++;
-					index++;
-				}
-				else if (nums.at(index) == 2)
-				{
-					nums.at(index) = nums.at(end);
-					nums.at(end) = 2;
-					end--;
-				}
-				else
-					index++;
-			}
-		}
+class Solution {
+public:
+    void sortColors(vector<int>& nums)
+    {
+        int start {0},index{0};
+        int end = nums.size()-1;
+        
+        while(index <= end)
+        {
+            if(nums[index] == 0)
+                swap(nums[index++],nums[start++]);
+            else
+                if(nums[index] == 2)
+                    swap(nums[index],nums[end--]);
+            else 
+                index++;
+        }
+    }
 };
