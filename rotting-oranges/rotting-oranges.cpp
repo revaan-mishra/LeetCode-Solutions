b92e0c8f50f4ed86a90cd0015f30ae6820ea4620
Revaan Mishra <76741350+revaan-mishra@users.noreply.github.com>
2021-02-28 03:41:00 +0530

    Time: 8 ms (76.98%), Space: 13.1 MB (67.97%) - LeetHub

---
+++ rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,71 @@
+class Solution
+{
+public:
+    auto orangesRotting(vector<vector<int>> &grid)
+    {
+        if (empty(grid))
+            return 0;
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        auto isValid = [m, n](int i, int j) {
+            if (i >= 0 && i < m && j >= 0 && j < n)
+                return true;
+            return false;
+        };
+
+        int rotten = 2, fresh = 1, empty = 0;
+        int freshCount {0};
+        int rottenCount {0};
+
+        queue<pair<int, int>> q;
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                auto idx = grid[row][col];
+
+                if (idx == rotten)
+                    q.push({row, col});
+                else if (idx == fresh)
+                    freshCount++;
+            }
+        }
+        
+        if(freshCount == 0)
+            return 0; 
+        
+        int time = 0;
+        vector<pair<int, int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
+        while (!q.empty())
+        {
+            int sz = q.size();
+
+            while (sz--)
+            {
+                auto nbr = q.front();
+                q.pop();
+
+                for (auto d : dir)
+                {
+                    int nextRow = nbr.first + d.first;
+                    int nextCol = nbr.second + d.second;
+                    
+                    if (isValid(nextRow, nextCol) and 
+                        grid[nextRow][nextCol] == fresh)
+                    {
+                        grid[nextRow][nextCol] = rotten;
+                        freshCount--;
+                        q.push({nextRow, nextCol});
+                    }
+                }
+            }
+            
+            time++;
+        }
+
+        return freshCount == 0 ? time - 1 : -1;
+    }
+};
