29fd0c872fe38486d4cc023b32a2a2895369ed87
Revaan Mishra <76741350+revaan-mishra@users.noreply.github.com>
2021-04-24 04:19:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (40.94%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        
-        int ans=0;
-        while (n>0)
-        {
-            ans=ans+(n&1);
-            n=n>>1;
-        }
-        return ans;
-    }
+    int hammingWeight(uint32_t n)
+    {
+        int count{ 0 };
+        while (n) {
+            n = (n & (n - 1));
+            count++;
+        }
+        return count;
+    }
 };
